c0a700c96e72b1cf43897f028247be49c832dc77
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-01-04 17:14:26 +0000

    hello world

---
+++ script.js
@@ -0,0 +1,6 @@
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello, World!';
+h1.className = 'greeting';
+h1.style.color = 'blue';
+h1.id = 'main-heading';
+document.getElementById('root').appendChild(h1);
